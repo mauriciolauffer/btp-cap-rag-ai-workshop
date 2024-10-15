ef42f4bd4aadfc3fe5cb8f7f87bb4048fae0ffab
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2024-10-15 20:14:01 +1100

    refactor: Leaves most of the code structure in place to guide DEVs

--- srv/chat-service.js
+++ srv/chat-service.js
@@ -1,11 +1,68 @@
 "use strict";
 
 const cds = require("@sap/cds");
+const chatHistoryInMemory = [];
+
+/**
+ * Get chat history session
+ */
+function getChatHistorySession(sessionId) {
+  throw new Error("ChatService getChatHistorySession has to be implemented!");
+}
+
+/**
+ * Get RAG response
+ */
+async function getRagResponse(userQuery, chatHistory) {
+  throw new Error("ChatService getRagResponse has to be implemented!");
+}
+
+/**
+ * Get the configuration to the embedding model
+ */
+function getAiEmbeddingConfig() {
+  throw new Error("ChatService getAiEmbeddingConfig has to be implemented!");
+}
+
+/**
+ * Get the configuration to the chat model
+ */
+function getAiChatConfig() {
+  throw new Error("ChatService getAiChatConfig has to be implemented!");
+}
+
+/**
+ * Prepare response from the AI
+ */
+function prepareResponse(ragResponse) {
+  throw new Error("ChatService prepareResponse has to be implemented!");
+}
+
+/**
+ * Add messages to the chat history session
+ */
+function addMessagesToChatHistory(sessionId, userContent, assistantContent) {
+  throw new Error("ChatService addMessagesToChatHistory has to be implemented!");
+}
 
 module.exports = class ChatService extends cds.ApplicationService {
   init() {
+    this.on("getAiResponse", async (req) => {
+      const userQuery = req.data?.content;
+      const chatHistory = getChatHistorySession(req.data.sessionId);
+      const ragResponse = await getRagResponse(userQuery, chatHistory);
+      const response = prepareResponse(ragResponse);
+      addMessagesToChatHistory(
+        req.data.sessionId,
+        userQuery,
+        response.content
+      );
+      return response;
+    });
 
-    throw new Error("ChatService has to be implemented!");
+    this.on("deleteChatSession", async (req) => {
+      throw new Error("ChatService deleteChatSession has to be implemented!");
+    });
 
     return super.init();
   }
--- srv/embedding-service.js
+++ srv/embedding-service.js
@@ -1,11 +1,64 @@
 "use strict";
 
 const cds = require("@sap/cds");
+const { RecursiveCharacterTextSplitter } = require("@langchain/textsplitters");
+const {
+  WebPDFLoader,
+} = require("@langchain/community/document_loaders/web/pdf");
+
+/**
+ * Get the configuration to the embedding model
+ */
+function getAiEmbeddingConfig() {
+  throw new Error("EmbeddingService getAiEmbeddingConfig has to be implemented!");
+}
+
+/**
+ * Create PDF blob with content from the table
+ */
+async function getPdfBlob(stream) {
+  throw new Error("EmbeddingService getPdfBlob has to be implemented!");
+}
+
+/**
+ * Split the document in multiple text chunks to be used in the embedding
+ */
+async function splitDocumentInTextChunks(pdfBlob) {
+  throw new Error("EmbeddingService splitDocumentInTextChunks has to be implemented!");
+}
+
+/**
+ * Convert text chunks to vector (embedding)
+ */
+async function getEmbeddingPayload(textChunks, filename) {
+  throw new Error("EmbeddingService getEmbeddingPayload has to be implemented!");
+}
+
+/**
+ * Embedding document process
+ */
+async function embeddingDocument(data, entities) {
+  throw new Error("EmbeddingService embeddingDocument has to be implemented!");
+}
+
+/**
+ * Convert embeddings to buffer, required to store it in SAP HANA tables
+ */
+function array2VectorBuffer(data) {
+  throw new Error("EmbeddingService array2VectorBuffer has to be implemented!");
+}
 
 module.exports = class EmbeddingService extends cds.ApplicationService {
   init() {
+    const { Files, DocumentChunk } = this.entities;
 
-    throw new Error("EmbeddingService has to be implemented!");
+    this.on("UPDATE", Files, async (req) => {
+      await embeddingDocument(req.data, this.entities);
+    });
+
+    this.on("deleteEmbeddings", async () => {
+      throw new Error("EmbeddingService deleteEmbeddings has to be implemented!");
+    });
 
     return super.init();
   }
